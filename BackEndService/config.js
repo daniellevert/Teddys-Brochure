aaec90d31b0055a364f9f8032516d55298afc424
daniel <47677702+daniellevert@users.noreply.github.com>
2021-09-07 11:15:32 -0500

    Update config.js

--- BackEndService/config.js
+++ BackEndService/config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	weatherAPIKey: '3dde8c8601fa351af855faf4239465e3',
 	weatherAPIUrl: 'https://api.openweathermap.org/data/2.5/onecall',
-	mongodbKey: 'mongodb+srv://sanchezkellyt31045:sOap&93036@nationalpark.utrjf.mongodb.net/TeddysBrochure?retryWrites=true&w=majority',
+	mongodbKey: '', // URI removed
 };
